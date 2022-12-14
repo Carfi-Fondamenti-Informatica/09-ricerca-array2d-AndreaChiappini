2147ccf1e5f2d9df2c550da696366baf1a31b67e
AndreaChiappini <117998815+AndreaChiappini@users.noreply.github.com>
2022-12-14 16:52:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char lista[10][20];
+    char nome [20];
+    int posizione = 0;
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 20; j++) {
+            nome[j] = ' ';
+            lista[i][j] = ' ';
+        }
+    }
+    for (int i = 0; i < 10; i++) {
+        cin >> lista[i];
+    }
+    cin >> nome;
+    if (ricerca(lista,nome,posizione)){
+        cout << posizione;
+    }else{
+        cout << "non presente" << endl;
+    }
+
+    return 0;
 }
+
