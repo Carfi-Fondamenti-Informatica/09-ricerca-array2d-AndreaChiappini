12d20785449b03ac0834406c8ebabbbbff1b3d1a
AndreaChiappini <117998815+AndreaChiappini@users.noreply.github.com>
2022-12-14 16:52:23 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,11 @@
+//
+// Created by Andrea Chiappini on 14/12/22.
+//
+
+#ifndef ESERCITAZIONE9_LIB_H
+#define ESERCITAZIONE9_LIB_H
+
+bool ricerca (char lista[10][20], char nome[20], int &posizione);
+
+#endif //ESERCITAZIONE9_LIB_H
 
