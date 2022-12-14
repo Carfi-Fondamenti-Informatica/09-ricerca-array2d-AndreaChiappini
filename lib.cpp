a56bb93d1fa7ad8fe6f9626817edd52d97926aa8
AndreaChiappini <117998815+AndreaChiappini@users.noreply.github.com>
2022-12-14 16:52:02 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,25 @@
+//
+// Created by Andrea Chiappini on 14/12/22.
+//
+
+#include "lib.h"
+
+bool ricerca (char lista[10][20], char nome[20], int &posizione) {
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 20; j++) {
+            if (lista[i][j] != nome[j]){
+                j = 20;
+            } else{
+                if (j==19){
+                    posizione = i;
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+
+
 
